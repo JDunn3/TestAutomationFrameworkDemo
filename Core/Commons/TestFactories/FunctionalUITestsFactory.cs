03c0130b60c33537a25ccb363bafe527b21735a6
Jeremy Dunn <jeremydunn123@gmail.com>
2018-03-02 12:44:25 -0600

    Add project files.

---
+++ Core/Commons/TestFactories/FunctionalUITestsFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAutomationFrameworkDemo.Core.Commons.TestObjects;
+
+namespace TestAutomationFrameworkDemo.Core.Commons.TestFactories
+{
+    public class FunctionalUITestsFactory : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { new FunctionalUITest() };
+        }
+    }
+}
